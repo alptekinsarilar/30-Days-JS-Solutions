1864952d9394aa1260093cee9ea636fb88f0b9d1
Alptekin <alptekinsarilar@gmail.com>
2023-01-09 18:43:11 +0300

    level 2 solutions

--- day10/index.js
+++ day10/index.js
@@ -1,5 +1,5 @@
 // Level 1
-const a = [4, 5, 8, 9];
+/* const a = [4, 5, 8, 9];
 const b = [3, 4, 5, 7];
 const countries = ["Finland", "Sweden", "Norway"];
 
@@ -24,4 +24,20 @@ console.log(secondSet);
 const countriesSet = new Map(
   countries.map((country) => [country, country.length])
 );
-console.log(countriesSet);
+console.log(countriesSet); */
+
+// Level 2
+const a = [4, 5, 8, 9];
+const b = [3, 4, 5, 7];
+const countries = ["Finland", "Sweden", "Norway"];
+
+let c = [...a, ...b];
+const unionSet = new Set(c);
+console.log(unionSet);
+
+const B = new Set(b);
+let intersection = a.filter((num) => B.has(num));
+const intersectionSet = new Set(intersection);
+console.log(intersectionSet);
+
+// Find a with b => I actually don't understand what that means
