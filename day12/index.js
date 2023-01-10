4c15a8492e7b48094a9eb0d7b59791420c3f5602
Alptekin <alptekinsarilar@gmail.com>
2023-01-10 14:12:31 +0300

    day12 level 2 solutions

--- day12/index.js
+++ day12/index.js
@@ -1,5 +1,5 @@
 // Level 1
-let text =
+/* let text =
   "He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.";
 let pattern = /\d+/g;
 let earnings = text.match(pattern);
@@ -21,3 +21,37 @@ console.log(is_valid_variable("first_name"));
 console.log(is_valid_variable("first-name"));
 console.log(is_valid_variable("1first_name"));
 console.log(is_valid_variable("firstname"));
+ */
+
+// Level 2
+const tenMostFrequentWords = (text) => {
+  let pattern = /\w+/g;
+  let words = text.match(pattern);
+  const map = new Map();
+  words.forEach((word) => {
+    if (map.get(word)) {
+      map.set(word, map.get(word) + 1);
+    } else {
+      map.set(word, 1);
+    }
+  });
+
+  const result = [];
+
+  for (const [key, value] of map) {
+    result.push({ word: key, count: value });
+  }
+
+  result
+    .sort((a, b) => {
+      if (a.count > b.count) return 1;
+      else if (a.count < b.count) return -1;
+      return 0;
+    })
+    .reverse();
+
+  return result.slice(0, 10);
+};
+
+let paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`;
+console.log(tenMostFrequentWords(paragraph));
