cb37955f11b78784ff12b3d74236827a346c0992
Alptekin <alptekinsarilar@gmail.com>
2023-01-10 19:37:47 +0300

    day13 level 3 solutions

--- day13/index.js
+++ day13/index.js
@@ -46,8 +46,31 @@ console.table(countries)
 console.groupEnd() */
 
 // Level 2
-console.assert(10 > 2 * 10, "10 is not bigger than 2 * 10");
+/* console.assert(10 > 2 * 10, "10 is not bigger than 2 * 10");
 
 console.warn("Warning! This is a warning message and you are in danger zone!");
 
 console.error("A ah! You should best check the MDN!");
+ */
+
+// Level 3
+
+import { countries as data } from "../data/countries.js";
+
+console.time("Regular for loop");
+for (let i = 0; i < data.length; i++) {
+  let j = data[i]; // for clear output I didn't print every country, instead I assign it to a variable
+}
+console.timeEnd("Regular for loop");
+
+console.time("for of loop");
+for (const country of data) {
+  let j = country;
+}
+console.timeEnd("for of loop");
+
+console.time("forEach loop");
+countries.forEach((country) => {
+  let j = country;
+});
+console.timeEnd("forEach loop");
