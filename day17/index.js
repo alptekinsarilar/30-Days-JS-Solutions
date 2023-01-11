970e61104076088f849fdefeecb883483eb637b9
Alptekin <alptekinsarilar@gmail.com>
2023-01-11 16:07:30 +0300

    day17 level 2 solutions

--- day17/index.js
+++ day17/index.js
@@ -1,7 +1,7 @@
 // Important note!: Run this codes on web console
 
 // Level 1
-console.log(localStorage);
+/* console.log(localStorage);
 
 localStorage.setItem("firstName", "Alptekin");
 localStorage.setItem("lastName", "Sarilar");
@@ -9,4 +9,17 @@ localStorage.setItem("age", "21");
 localStorage.setItem("country", "Turkey");
 localStorage.setItem("city", "Ankara");
 
+console.log(localStorage); */
+
+// Level 2
+const student = {
+  firstName: "Alptekin",
+  lastName: "Sarilar",
+  age: 21,
+  skills: ["HTML", "JavaScript"],
+  country: "Turkey",
+};
+
+localStorage.setItem("student", JSON.stringify(student, undefined, 4));
 console.log(localStorage);
+console.log(localStorage.getItem("student"));
