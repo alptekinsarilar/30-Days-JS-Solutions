29a64da2e31c34e17919a5d394fb6162fe0f4a3e
Alptekin <alptekinsarilar@gmail.com>
2023-01-11 15:59:05 +0300

    day17 level 1 solutions

---
+++ day17/index.js
@@ -0,0 +1,12 @@
+// Important note!: Run this codes on web console
+
+// Level 1
+console.log(localStorage);
+
+localStorage.setItem("firstName", "Alptekin");
+localStorage.setItem("lastName", "Sarilar");
+localStorage.setItem("age", "21");
+localStorage.setItem("country", "Turkey");
+localStorage.setItem("city", "Ankara");
+
+console.log(localStorage);
