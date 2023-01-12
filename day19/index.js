efe2b6f58042d367bc310a1d6ed7974cd1cb2331
Alptekin <alptekinsarilar@gmail.com>
2023-01-12 14:28:46 +0300

    day19 level 2 solutios

--- day19/index.js
+++ day19/index.js
@@ -1,7 +1,7 @@
 // Closures remember the outer function scope even after creation time.
 
 // Level 1
-function wait(ms) {
+/* function wait(ms) {
   return function (message) {
     setTimeout(() => {
       console.log(message);
@@ -13,4 +13,30 @@ const wait500 = wait(500);
 const wait1000 = wait(1000);
 
 wait500("Hi");
-wait1000("Hello");
+wait1000("Hello"); */
+
+// Level 2
+function guessTheRandom() {
+  let random = Math.floor(Math.random() * 100);
+  function isLessThan(num) {
+    return random < num;
+  }
+  function isEqual(num) {
+    return random === num;
+  }
+  function printRandom() {
+    console.log(random);
+  }
+
+  return {
+    isLessThan,
+    isEqual,
+    printRandom,
+  };
+}
+
+const innerFunctions = guessTheRandom();
+console.log(innerFunctions.isLessThan(15));
+console.log(innerFunctions.isLessThan(30));
+console.log(innerFunctions.isEqual(64));
+innerFunctions.printRandom();
